2c2fed7e97ccc9027549d4dbc38d1467aa72d417
FARHAN X0VAR <farhanx0var@FARHANs-MacBook-Air.local>
2025-03-14 00:26:43 +0600

    database connected successfuly with moongose

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-
-app.listen(3000, () => {
-    console.log("Hello, it's working");
-});
 
 app.get('/', (req, res) => {
     res.send('The Api Is Working Well Buddy ;-)');
 });
+
+mongoose.connect('mongodb+srv://admin:admin@crud.omfmm.mongodb.net/?retryWrites=true&w=majority&appName=CRUD')
+    .then(() => {
+        console.log("Connected To The Database Successfully");
+        app.listen(3000, () => {
+            console.log("Hello, it's working");
+        });
+    })
+    .catch((err) => {
+        console.log("Something went wrong", err);
+    });
